439e3e5a752b41f85099abb4d63ab83ab736f033
josef <josef.vancura@email.cz>
2021-04-06 12:49:27 +0200

    1st commit to MASTER

---
+++ app/src/main/java/cz/vancura/branches2/MainActivity.java
@@ -0,0 +1,30 @@
+package cz.vancura.branches2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "myTAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Log.d(TAG, "onCreate");
+
+
+        // step 1 - MASTER - empty android project
+        // step 2 - branch 1 - add MVVM
+        // step 3 - merge branch 1 to MASTER
+
+
+
+
+
+    }
+}
